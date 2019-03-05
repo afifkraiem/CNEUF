c7283807ff112ad7513bf5db700ccef430a79979
syrinech94 <chehidasyrine@gmail.com>
2019-03-05 11:36:21 +0100

    tablearray

--- src/app/coord/soumission/soumission.component.ts
+++ src/app/coord/soumission/soumission.component.ts
@@ -43,7 +43,8 @@ candidature: Candidature;
         academiques: this.fb.array([this.createPart()]),
         economiques: this.fb.array([this.createPart()]),
         autres: this.fb.array([this.createPart()]),
-        implantations: this.fb.array([this.createPart()])
+        implantations: this.fb.array([this.createPart()]),
+        partenaires: this.fb.array([this.createinst()]) 
 
       });
       if (this.tokenstorage.getToken()) {
@@ -51,6 +52,24 @@ candidature: Candidature;
         console.log(this.username);
       }
     }
+    createinst() {
+      return this.fb.group ({
+      fpa1: '',
+      fpa2: '',
+      fpa3: '',
+      fpa4: '',
+      ipa1: '',
+      ipa2: '',
+      ipa3: '',
+      ipa4: '',
+      ppa1: '',
+      ppa2: '',
+      ppa3: '',
+      ppa4: ''
+      
+      });
+      } 
+
     createPart() {
       return this.fb.group({
         institution: '',
@@ -75,8 +94,16 @@ candidature: Candidature;
     addParts() {
     const acad = <FormArray>this.form.controls.academiques as FormArray;
       acad.push(this.createPart() );
-
+     
+    const part = <FormArray>this.form.controls.partenaires as FormArray;
+    part.push(this.createinst() ); 
+ 
     }
+
+  get partesnaires() {
+    return this.form.get('partenaires') as FormArray;
+    } 
+
   removePart(index) {
     this.acadForms.removeAt(index);
   }
@@ -88,6 +115,9 @@ candidature: Candidature;
   addEco() {
   const eco = <FormArray>this.form.controls.economiques as FormArray;
     eco.push(this.createPart());
+    const part = <FormArray>this.form.controls.partenaires as FormArray;
+    part.push(this.createinst() ); 
+ 
 }
 
 removeEco(index) {
@@ -100,6 +130,9 @@ get autForms() {
 addAut() {
 const aut = <FormArray>this.form.controls.autres as FormArray;
   aut.push(this.createPart());
+  const part = <FormArray>this.form.controls.partenaires as FormArray;
+    part.push(this.createinst() ); 
+ 
 }
 removeAut(index) {
 this.autForms.removeAt(index);
@@ -112,18 +145,19 @@ get implForms() {
 addImpl() {
 const impl = <FormArray>this.form.controls.implantations as FormArray;
   impl.push(this.createPart());
+  const part = <FormArray>this.form.controls.partenaires as FormArray;
+    part.push(this.createinst() ); 
+ 
 }
 removeImpl(index) {
 this.implForms.removeAt(index);
 }
 
 Soumettre() {
-  console.log(this.form.value);
+  const frm = JSON.stringify(JSON.parse(this.form.value));
+  console.log(frm);
   console.log('hello');
-  this.candserice.saveCand(this.form.value).subscribe(
-    data => console.log('success!', data),
-    error => console.log('error', error)
-  );
+    
   }
   }
 
